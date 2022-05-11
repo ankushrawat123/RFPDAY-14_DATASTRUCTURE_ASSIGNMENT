64bbbdb6a7e80f1e7c31d232342f0853cc265543
AnkushRawat <ankushrwt344@gmail.com>
2022-05-11 07:34:12 +0530

    Uploading SimpleLinkedList file in Uc1 branch

--- RFPDAY-14_DATASTRUCTURE_ASSIGNMENT/Program.cs
+++ RFPDAY-14_DATASTRUCTURE_ASSIGNMENT/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RFPDAY_14_DATASTRUCTURE_ASSIGNMENT;
+Console.WriteLine("WELCOME TO LINKED LIST PROGRAM!\n");
+SimpleLinkedList linkedlistobj = new SimpleLinkedList();
+linkedlistobj.simpleLinkedList();
+Console.WriteLine("Simple Linked List of 56, 30, 70");
+linkedlistobj.display();
---
+++ RFPDAY-14_DATASTRUCTURE_ASSIGNMENT/SimpleLinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFPDAY_14_DATASTRUCTURE_ASSIGNMENT
+{
+    internal class SimpleLinkedList
+    {
+        LinkedList<int> myList= new LinkedList<int>();
+        public void simpleLinkedList()
+        {
+            myList.AddLast(56);
+            myList.AddLast(30);
+            myList.AddLast(70);
+
+        }
+        public void display()
+        {
+            foreach(int i in myList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
